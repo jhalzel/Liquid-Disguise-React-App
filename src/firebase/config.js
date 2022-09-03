d4eb24d4377ff0a437edff764795b9279825c006
Jeremy Halzel <jhalzel@worcester.edu>
2022-09-03 14:46:01 -0400

    minor image modifications

---
+++ src/firebase/config.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDdSwxyUFMGxjy05NFbUJ4yQqft48yfcwQ",
+  authDomain: "liquid-disguise-eabf8.firebaseapp.com",
+  projectId: "liquid-disguise-eabf8",
+  storageBucket: "liquid-disguise-eabf8.appspot.com",
+  messagingSenderId: "816906038246",
+  appId: "1:816906038246:web:8258fe08de7bb71ad34427",
+};
+
+//init firebase
+initializeApp(firebaseConfig);
